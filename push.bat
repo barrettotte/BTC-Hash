5033943e0b7d3f4a211fb35de96b2e6c5fd052aa
Barrett Otte <Barrett.Otte@goodville.com>
2019-08-18 18:51:11 -0400

    Small changes to push scripts

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 IF [%1] == [] GOTO NOMSG
-python3 ibmi-pull.py && git add . && git commit -m "%1" && git push origin master
+python ibmi-pull.py && git add . && git commit -m "%~1" && git push origin master
 GOTO END
 :NOMSG
   ECHO "Enter the commit message!"
--- push.sh
+++ push.sh
@@ -3,7 +3,11 @@ if [ $# -eq 0 ]; then
   echo "Enter the commit message!"
   exit
 fi
-python3 ibmi-pull.py 
-git add . 
-git commit -m "${1}" 
-git push origin master
+python3 ibmi-pull.py
+if [ $? -eq 0 ]; then
+  git add . 
+  git commit -m "${1}" 
+  git push origin master
+else
+  echo "Something went wrong, no push"
+fi
